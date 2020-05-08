db5c1bd351d1ccc8345bfc08baeab52d25b42d09
Morten Lenschow Hansen <au616040@uni.au.dk>
2020-05-08 14:22:07 +0200

    Errors in weather controller get()

---
+++ WeatherStation/Controllers/WeatherForecastController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WeatherStation.Models;
+using WeatherStation.Service;
+
+namespace WeatherStation.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private DbContext _context;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IDbContextSettings context, IWeatherCollection weatherCollection)
+        {
+            _logger = logger;
+
+            _context = new DbContext(context, weatherCollection);
+        }
+
+        [HttpGet]
+        public async ActionResult<List<Weather>> Get()
+        {
+            return await _context.GetForecasts();
+        }
+    }
+}
