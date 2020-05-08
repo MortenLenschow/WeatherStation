5acf55eec76f8b06084fbf7da6774445c99cc27d
Morten Lenschow Hansen <au616040@uni.au.dk>
2020-05-08 11:21:54 +0200

    added model  and data folder

---
+++ WeatherStation/Models/Weather.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeatherStation.Models
+{
+    public class Weather
+    {
+        public DateTime date { get; set; }
+        public int TemperatureC { get; set; }
+        public string Summary { get; set; }
+    }
+}
